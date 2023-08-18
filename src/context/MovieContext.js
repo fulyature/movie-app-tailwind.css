cec1311ec7cfc707cfc4b2e3875740f19fda980d
fulya_ture <fulyature@gmail.com>
2023-08-18 12:12:05 +0300

    setMovies,setLoading

--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -1,8 +1,33 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const MovieContext = createContext();
 
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
 const MovieContextProvider = ({ children }) => {
-  return <MovieContext.Provider value={null}>{children}</MovieContext.Provider>;
+  const [movies, setMovies] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    setLoading(true);
+    axios
+      .get(API)
+      .then((res) => console.log(res.data.result))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  };
+
+  return (
+    <MovieContext.Provider value={{ movies, getMovies, loading }}>
+      {children}
+    </MovieContext.Provider>
+  );
 };
 export default MovieContextProvider;
